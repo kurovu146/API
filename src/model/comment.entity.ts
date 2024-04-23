64e40db234c2272514961b9ec9a7e82355f4bdad
Vũ Đức Tuấn (HN-BSD-Developer) <tuanvd@runsystem.local>
2024-04-23 16:46:20 +0700

    first commit

---
+++ src/model/comment.entity.ts
@@ -0,0 +1,28 @@
+import { BelongsTo, Column, ForeignKey, Table } from 'sequelize-typescript';
+import { TEXT } from 'sequelize';
+import { Question } from 'src/model/question.entity';
+import { User } from 'src/model/user.entity';
+import { Base } from './base.entity';
+
+@Table
+export class Comment extends Base {
+  @Column({type: TEXT})
+  content: string;
+
+  @Column({ field: 'comment_id' })
+  commentId?: number;
+
+  @ForeignKey(() => User)
+  @Column({ field: 'user_id' })
+  userId: number;
+
+  @ForeignKey(() => Question)
+  @Column({ field: 'question_id' })
+  questionId: number;
+
+  @BelongsTo(() => User)
+  user: User;
+
+  @BelongsTo(() => Question)
+  question: Question;
+}
